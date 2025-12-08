d7505f51ac82075726de8a27514ae8f38e4f3e04
Sagos4321 <gasosart@gmail.com>
2025-12-08 20:37:59 +0800

    tambahin graph, belom kepakek sih hehe

--- src/ArcadiaAscension.java
+++ src/ArcadiaAscension.java
@@ -2,22 +2,42 @@ import java.util.Scanner;
 
 import level.level1.Level1MemoryGame;
 import player.PlayerNode;
+import utils.ClearScreen;
 import level.BossLevel.LevelBoss;
 import level.BossLevel.NodeBoss;
+import level.Map.Graph;
 
 public class ArcadiaAscension {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Graph graph = new Graph(); 
+        
+
+        graph.addVertex(0, "Entry"); 
+        graph.addVertex(1, "Ruins Hill"); 
+        graph.addVertex(2, "Labyrinth Tower"); 
+        graph.addVertex(3, "Dora Mountain"); 
+        graph.addVertex(4, "Forest"); 
+        graph.addVertex(5, "Hollow Earth"); 
+        graph.addVertex(6, "The Eternal Relic"); 
+        graph.addVertex(7, "Monster Island"); 
+        graph.addVertex(8, "Safety Room"); 
+        graph.addVertex(9, "Village"); 
+        graph.addEdge(0, 6, 4);graph.addEdge(0, 2, 8); 
+        graph.addEdge(0, 3, 7);graph.addEdge(1, 4, 2); 
+        graph.addEdge(1, 7, 5);graph.addEdge(1, 3, 8); 
+        graph.addEdge(1, 8, 9);graph.addEdge(3, 4, 1); 
+        graph.addEdge(3, 6, 3);graph.addEdge(3, 5, 9); 
+        graph.addEdge(5, 9, 2);graph.addEdge(5, 8, 10); 
+        graph.addEdge(2, 5, 6); 
+        
+
         mainMenu(input);
+
     }
 
     static void mainMenu(Scanner input) {
         int menuSekarang = 1;
-        PlayerNode player = new PlayerNode("Gasos", 350, 50);
-        NodeBoss boss = new NodeBoss("Dark Lord", 1000, 75);
-
-        LevelBoss bossLevel = new LevelBoss(player, boss);
-        Level1MemoryGame level1 = new Level1MemoryGame(player, bossLevel);
 
         while (menuSekarang != 0) {
             cls();
@@ -51,7 +71,7 @@ public class ArcadiaAscension {
                 case 3:
                     switch (menuSekarang) {
                         case 1:
-                            level1.start();
+                            StartGame(input);
                             break;
                         case 2:
                             break;
@@ -76,6 +96,34 @@ public class ArcadiaAscension {
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    static void StartGame(Scanner input){
+        ClearScreen.clearScreen();
+        Banner();
+        System.out.println("======================================================");
+        System.out.println("\t\tWELCOME PLAYER");
+        System.out.print("Masukan nama anda: ");
+        String tes = input.nextLine();//buang buffer yang tertahan
+        String playerName = input.nextLine();
+        System.out.print("Hallo " + playerName);
+
+        
+        NodeBoss boss = new NodeBoss("Dark Lord", 1000, 75);
+        PlayerNode player = new PlayerNode(playerName, 250, 75);
+        LevelBoss bossLevel = new LevelBoss(player, boss);
+        Level1MemoryGame level1 = new Level1MemoryGame(player, bossLevel);
+
+        for (int i = 0; i < 15; i++) {
+            try {
+                Thread.sleep(250);
+            } catch (Exception e) {
+            }
+            System.out.print(".");
+        }
+        
+        level1.start();
+
     }
 
     private static void Banner() {
---
+++ src/level/Map/Graph.java
@@ -0,0 +1,126 @@
+package level.Map;
+
+public class Graph {
+    Vertex head;
+
+    public void addVertex(int id, String name) { 
+        Vertex newVertex = new Vertex(id, name); 
+        if (head == null) {head = newVertex; 
+        }else {
+            Vertex temp = head; 
+            while (temp.next != null) {temp = temp.next;} 
+            temp.next = newVertex;
+        }
+    } 
+    public int returnID(String name){ 
+        Vertex temp = head; 
+        while(temp != null){ 
+            if(temp.name.equalsIgnoreCase(name)){ 
+                return temp.id;
+            }
+            temp = temp.next;
+        }
+        return 9999;
+    }
+
+    public String findById(int id){ 
+        Vertex temp = head; 
+        while(temp != null){
+            if(temp.id == id){
+                return temp.name;
+            } 
+            temp = temp.next;
+        }
+        return " ";
+    }
+
+    public Vertex findVertex(int id) { 
+        Vertex temp = head; 
+        while (temp != null) {
+            if (temp.id == id) {
+                return temp;
+            } 
+            temp = temp.next;
+        }
+        return null;
+    } 
+
+    public Vertex findVertexByName(String name) { 
+        Vertex temp = head; 
+        while (temp != null) { 
+            if (temp.name.equalsIgnoreCase(name)) { 
+                return temp; 
+            } 
+            temp = temp.next; 
+        } 
+        return null; 
+    }
+    
+    public void addEdge(int source, int destination, int weight) { 
+        Vertex sourceVertex = findVertex(source); 
+        Vertex destinationVertex = findVertex(destination); 
+        if (sourceVertex != null && destinationVertex != null) { 
+            sourceVertex.addEdge(destination, weight); 
+            destinationVertex.addEdge(source, weight); 
+        } 
+    } 
+
+    public void displayAdjacentLocations(String currentLocation) { 
+        Vertex currentVertex = findVertexByName(currentLocation); 
+        if (currentVertex == null) { 
+            System.out.println("Location not found."); 
+            return; 
+        } 
+        Edge edge = currentVertex.adjacencyList; 
+        while (edge != null) { 
+            Vertex neighbor = findVertex(edge.destination); 
+            if (neighbor != null) { 
+                System.out.println("- " + " Point " + 
+                neighbor.id + " (Distance: " + edge.weight + ")"); 
+            } 
+            edge = edge.next;  
+        } 
+    } 
+
+    public int getDistanceBetween(String currentLocation, String destinationName) { 
+        Vertex currentVertex = findVertexByName(currentLocation); 
+        if (currentVertex == null) { 
+            return -1; 
+        } 
+        Edge edge = currentVertex.adjacencyList; 
+        while (edge != null) { 
+            Vertex neighbor = findVertex(edge.destination); 
+            if 
+            (neighbor != null && neighbor.name.equalsIgnoreCase(destinationName)) { 
+                return edge.weight; 
+            } 
+            edge = edge.next; 
+        } 
+        return -1; 
+    }
+
+    public void displayCurrentLocation(String currentLocation) { 
+        System.out.println("\nCurrent Location: " + currentLocation);
+    } 
+
+    public boolean isValidDestination(String currentLocation, String destination) { 
+        Vertex currentVertex = findVertexByName(currentLocation); 
+        if (currentVertex == null) {return false;} 
+        Edge edge = currentVertex.adjacencyList; 
+        while (edge != null) { 
+            Vertex neighbor = findVertex(edge.destination); 
+            if (neighbor != null && neighbor.name.equalsIgnoreCase(destination)) { return true;} 
+            edge = edge.next;
+        } 
+        return false;
+    } 
+
+    public void resetGraph() { 
+        Vertex current = head; 
+        while (current != null) { 
+            current.distance = Integer.MAX_VALUE; 
+            current.visited = false;current.prev = null; 
+            current = current.next;
+        }
+    } 
+}

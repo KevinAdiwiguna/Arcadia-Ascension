2fd8a53c3d711b67bda58156d971ff17670487e9
echa <raissabunga04@gmail.com>
2025-12-12 12:10:50 +0800

    Changes to be committed: 	modified:   src/level/Level4/Level4.java 	new file:   src/level/Level4/Linkedlistlvl4.java 	new file:   src/level/Level4/Nodelvl4.java

--- src/level/Level4/Level4.java
+++ src/level/Level4/Level4.java
@@ -14,7 +14,12 @@ public class Level4 {
 
     public void start() {
         Scanner sc = new Scanner(System.in);
-        int[] password = { 4, 9, 2, 7, 5 };
+       Linkedlistlvl4 password = new Linkedlistlvl4();
+        password.insertLast(4);
+        password.insertLast(9);
+        password.insertLast(2);
+        password.insertLast(7);
+        password.insertLast(5);
 
         while (player.getCurrentHp() > 0) {
             System.out.println("\n=====================================================================================");
@@ -23,30 +28,30 @@ public class Level4 {
 
             System.out.print("Masukkan 5 digit (pisah spasi): ");
 
-            int[] guess = new int[5];
+           Linkedlistlvl4 guess = new Linkedlistlvl4();
 
             // input 5 angka sekaligus pakai spasi
-            for (int i = 0; i < 5; i++) {
+           for (int i = 0; i < 5; i++) {
                 try {
-                    guess[i] = sc.nextInt();
+                    guess.insertLast(sc.nextInt());
                 } catch (Exception e) {
-                    System.out.println("Input tidak valid! Masukkan angka saja.");
+                    System.out.println("Input ga valid, masukkin angka ya.");
                     sc.nextLine();
-                    i--;  // ulangi input angka yang salah
+                    i--;
                 }
             }
 
             // Copy + Sorting
-            int[] passSorted = copy(password);
-            int[] guessSorted = copy(guess);
-            bubbleSort(passSorted);
-            bubbleSort(guessSorted);
+            Linkedlistlvl4 passSorted = password.copy();
+            Linkedlistlvl4 guessSorted = guess.copy();
+            passSorted.bubbleSort();
+            guessSorted.bubbleSort();
 
             char[] symbol = new char[5];
 
             // Cek posisi benar
-            for (int i = 0; i < 5; i++) {
-                if (guess[i] == password[i]) {
+           for (int i = 0; i < 5; i++) {
+                if (guess.get(i) == password.get(i)) {
                     symbol[i] = 'O';
                 }
             }
@@ -54,11 +59,8 @@ public class Level4 {
             // Cek posisi salah (Y)
             for (int i = 0; i < 5; i++) {
                 if (symbol[i] != 'O') {
-                    if (contains(passSorted, guess[i])) {
-                        symbol[i] = 'Y';
-                    } else {
-                        symbol[i] = 'X';
-                    }
+                    if (passSorted.contains(guess.get(i))) symbol[i] = 'Y';
+                    else symbol[i] = 'X';
                 }
             }
 
@@ -66,58 +68,33 @@ public class Level4 {
             System.out.println();
             System.out.println("X = SALAH | Y = ADA TAPI POSISI SALAH | O = BENAR POSISI\n");
 
-            for (int n : guess) System.out.print(n + " ");
+             for (int i = 0; i < 5; i++) System.out.print(guess.get(i) + " ");
             System.out.println();
+
             for (char s : symbol) System.out.print(s + " ");
             System.out.println("\n");
 
             // Cek menang
-            boolean semuaBenar = true;
+           boolean menang = true;
             for (char c : symbol) {
-                if (c != 'O') semuaBenar = false;
+                if (c != 'O') menang = false;
             }
 
-            if (semuaBenar) {
+            if (menang) {
                 System.out.println("Password benar! Pintu terbuka!");
                 delay(25, 150);
                 return;
             }
 
-            // Salah → kena damage
             player.getDamage(25);
-            System.out.println("Password salah! Anda menerima damage! HP sekarang: " + player.getCurrentHp());
+            System.out.println("Password salah! Kena damage! HP sekarang: " + player.getCurrentHp());
             delay(25, 100);
         }
 
-        System.out.println("Kesempatan habis. Pintu tetap terkunci.");
+        System.out.println("Kesempatan habis. Pintu tetep kekunci.");
         delay(25, 100);
     }
-
-    private void bubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-    }
-
-    private boolean contains(int[] arr, int target) {
-        for (int x : arr) {
-            if (x == target) return true;
-        }
-        return false;
-    }
-
-    private int[] copy(int[] arr) {
-        int[] result = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) result[i] = arr[i];
-        return result;
-    }
-
+    
     static void delay(int length, int ms) {
         for (int i = 0; i < length; i++) {
             try { Thread.sleep(ms); } catch (Exception e) {}

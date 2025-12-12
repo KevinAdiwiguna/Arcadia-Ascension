e0eddc64a374b19d204792c70a58af69925b7f85
KomangAyuw <ayusumaitri06@gmail.com>
2025-12-12 13:29:07 +0800

    perbaiki level 3 bagian correctStack

--- src/komang/MainGame.java
+++
@@ -1,8 +0,0 @@
-package komang;
-public class MainGame {
-    public static void main(String[] args) {
-        PlayerNode player = new PlayerNode("Hero", 100, 10);
-        Level3Game level = new Level3Game(player);
-        level.start();
-    }
-}
